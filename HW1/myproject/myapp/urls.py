a1a148a66d86b7e201ddcada663f7a2cd235d21f
MrVoropaev <mrvoropaev@gmail.com>
2024-03-18 11:53:41 +0300

    HW6v3

--- HW1/myproject/myapp/urls.py
+++ HW1/myproject/myapp/urls.py
@@ -4,3 +4,4 @@ from .views import client_orders_list
 urlpatterns = [
     path('client_orders/<int:client_id>/', client_orders_list, name='client_orders_list'),
 ]
+
--- HW1/myproject/myproject/settings.py
+++ HW1/myproject/myproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,10 +54,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'myproject.urls'
 
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'myapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
